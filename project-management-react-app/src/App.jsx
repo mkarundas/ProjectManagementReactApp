27799be9d74104172c666cf4147496e54c2dfb39
Arundas MK <mkarundas@gmail.com>
2024-03-28 19:05:26 +0530

    makeing project selectable

--- project-management-react-app/src/App.jsx
+++ project-management-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
 
@@ -10,6 +11,15 @@ function App() {
     selectedProjectId: undefined,
     projects: []
   });
+
+  function handleSelectProject(id) {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id
+      }
+    });
+  }
 
   function handleStartAddProject() {
     setProjectState(prevState => {
@@ -43,18 +53,18 @@ function App() {
       }
     });
   }
-
-  let content;
+  const selectedProject = projectState.projects.find(project=> project.id === projectState.selectedProjectId);
+  let content = <SelectedProject project={selectedProject}/>;
 
   if(projectState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
-  } else {
+  } else if (projectState.selectedProjectId === undefined){
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
   }
 
   return (
    <main className="h-screen my-8 flex gap-8">
-    <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectState.projects}/>
+    <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectState.projects} onSelectProject={handleSelectProject}/>
     {content}
    </main>
   );
